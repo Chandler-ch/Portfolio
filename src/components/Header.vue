4fcd97dc761b6be302596897de5b106c86ddf749
Chandler-ch <rchandler@mail.ch>
2026-09-23 13:48:22 +0200

    AME-11: added a dev header

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <img class="dev-picture" src="./../assets/dev-header2.jpeg" />
+</template>
+<style scoped>
+.dev-picture {
+  width: 10000px;
+  height: 100%;
+  object-fit: cover; /* Schneidet das Bild passend zu */
+  object-position: center; /* Bestimmt den Fokuspunkt (z.B. top, bottom, center)*/
+}
+</style>
