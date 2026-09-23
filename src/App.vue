ca8055e2b3534e4c81363f455320928bfc5ac0dc
Chandler-ch <rchandler@mail.ch>
2026-09-23 14:17:18 +0200

    AME-11: moved the header element to the header component

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,8 @@ import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <header class="site-header">
-    <Header></Header>
-  </header>
+  <Header></Header>
   <body></body>
 </template>
 
-<style scoped>
-.site-header {
-  width: 100vw;
-  height: 20vh;
-  margin-left: calc(-50vw + 50%);
-  margin-top: calc(-50vw + 50%);
-  overflow: visible;
-  position: relative;
-}
-</style>
+<style scoped></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts"></script>
 <template>
-  <img class="dev-picture" src="./../assets/dev-header2.jpeg" />
+  <header class="container">
+    <img class="dev-picture" src="./../assets/dev-header2.jpeg" />
+  </header>
 </template>
 <style scoped>
+.container {
+  width: 100vw;
+  height: 20vh;
+  margin-left: calc(-50vw + 50%);
+  margin-top: calc(-50vw + 50%);
+  overflow: visible;
+  position: relative;
+}
 .dev-picture {
   width: 100%;
   height: 100%;
